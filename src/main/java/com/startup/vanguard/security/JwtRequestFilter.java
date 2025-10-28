037bd9ab2874a5970fc6de71eceeb64b6b6943f7
luizhenrique <henriqueluiz1103@gmail.com>
2025-10-28 19:19:23 -0300

    fix:Adicionado ignore Options para preflight passarem direto

--- src/main/java/com/startup/vanguard/security/JwtRequestFilter.java
+++ src/main/java/com/startup/vanguard/security/JwtRequestFilter.java
@@ -36,6 +36,12 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         try {
             final String authorizationHeader = request.getHeader("Authorization");
 
