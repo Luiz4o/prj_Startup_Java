aa36309d58d787d7f31dbff7d496d539c01cea61
luizhenrique <henriqueluiz1103@gmail.com>
2025-10-28 19:11:54 -0300

    fix:Cors configurações

--- src/main/java/com/startup/vanguard/security/SecurityConfig.java
+++ src/main/java/com/startup/vanguard/security/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/usuario/create").permitAll()
