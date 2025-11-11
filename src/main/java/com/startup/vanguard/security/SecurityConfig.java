17b5f940036e4b2f34c2f75765fe854682d172b1
Luiz4o <henriqueluiz1103@gmail.com>
2025-11-11 20:48:28 -0300

    feat: adicionado link de produção do app

--- src/main/java/com/startup/vanguard/security/SecurityConfig.java
+++ src/main/java/com/startup/vanguard/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "https://startup-vanguard.vercel.app/*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
